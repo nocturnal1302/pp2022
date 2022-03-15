bf9f8b25a2c5d5611527b19ebb82e34ac874267e
Trinh Tu <47326110+nocturnal1302@users.noreply.github.com>
2022-03-16 01:21:01 +0700

    Add Student mark management system

---
+++ 1.student.mark.py
@@ -0,0 +1,21 @@
+# FUNCTIONS
+def list_student():
+  # for loop
+  # loop through list, print items
+  # if empty, prompt to add student
+
+def list_course():
+
+def add_student():
+  # input student info
+  name = input('Student name: ')
+  id = input('Student ID: ')
+  DoB = input('Date of Birth: ')
+  
+  # also 
+
+student_info = {
+  'Name' : name,
+  'ID' : id,
+  'DoB': DoB
+}
