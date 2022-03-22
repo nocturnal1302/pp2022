500aa977aed997814dc218105851e5f025133e25
Trinh Tu <tutrinh1302.memes@gmail.com>
2022-03-22 23:44:26 +0700

    Add function headers

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,21 +1,37 @@
-# FUNCTIONS
-def list_student():
-  # for loop
-  # loop through list, print items
-  # if empty, prompt to add student
-
-def list_course():
+def input_student_count():
+    pass
 
 def add_student():
-  # input student info
-  name = input('Student name: ')
-  id = input('Student ID: ')
-  DoB = input('Date of Birth: ')
-  
-  # also 
+    # input student info
+    name = input('Student name: ')
+    id = input('Student ID: ')
+    DoB = input('Date of Birth: ')
 
-student_info = {
-  'Name' : name,
-  'ID' : id,
-  'DoB': DoB
-}
+    # add to student dict
+
+def add_course_count():
+    pass
+
+def add_course():
+    # input course info
+    # add to course dict
+    pass
+
+def select_course():
+    # choose from course dict
+
+def list_course():
+    pass
+
+def list_student():
+    # for loop
+    # loop through list, print items
+    # if empty, prompt to add student
+    pass
+
+def show_mark():
+    pass
+
+
+
+add_student()
