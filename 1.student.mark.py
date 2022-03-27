126a4c5075c2c2b7bb4973d4590771af7a039679
Trinh Tu <tutrinh1302.memes@gmail.com>
2022-03-28 00:00:45 +0700

    implement try except and input validation in input function

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,12 +1,27 @@
-studentCount = int(input('Enter number of students: '))
-if studentCount > 0:
-    pass
-else:
-    print('Number of students must be bigger than 0.')
-    quit()
+def get_number(prompt):  # function for entering number
+    while True:  # input validation
+        try:
+            value = int(input(prompt))
+        except ValueError:
+            print("Please enter a positive integer number.")
+            continue
+
+        if value <= 0:
+            print("Number must be greater than 0")
+            continue
+        else:
+            break
+    return value
+
 
 student_list = []
 course_list = []
+
+
+def input_number_of_student():
+    global studentCount
+    studentCount = get_number('Enter number of students: ')
+
 
 def add_student():
     for i in range(studentCount):
@@ -20,30 +35,47 @@ def add_student():
         student_list.append(student)  # add tuple as an element to end of list
         print('\n')
 
-def add_course_count():
-    pass
+
+def input_number_of_courses():
+    global courseCount
+    courseCount = get_number('Enter number of courses: ')
+
 
 def add_course():
-    # input course info
-    # add to course dict
-    pass
+    for i in range(courseCount):
+        print(f'Enter information for course {i + 1}:')
+        id = input('- Course ID: ')
+        name = input('- Course name: ')
+
+        course = (id, name)
+
+        course_list.append(course)  # add tuple as an element to end of list
+        print('\n')
+
 
 def input_mark_for_course():
     # choose from course dict
     pass
 
+
 def list_course():
-    pass
+    [print(record) for record in course_list]
+
 
 def list_student():
     # for loop
     # loop through list, print student names
-    # if empty, prompt to add student
-    [print(records) for records in student_list]
+    [print(record) for record in student_list]
+
 
 def show_mark():
     pass
 
 
+input_number_of_student()
 add_student()
 list_student()
+
+input_number_of_courses()
+add_course()
+list_course()
