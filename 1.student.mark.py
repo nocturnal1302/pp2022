ba6ab386a1d4bc6e9a4628d712c460516c7bd4ef
Trinh Tu <tutrinh1302.memes@gmail.com>
2022-04-05 23:36:12 +0700

    update practical work 1

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,56 +1,57 @@
-def get_number(prompt):  # function for entering number
+def get_number(prompt):  # function for inputting number of students or courses
     while True:  # input validation
         try:
             value = int(input(prompt))
+
+        # no error when user input a string, a float, etc
         except ValueError:
-            print("Please enter a positive integer number.")
+            print("Please enter an integer number!")
             continue
 
         if value <= 0:
-            print("Number must be greater than 0")
+            print("Value must be greater than 0.")
             continue
         else:
             break
     return value
 
 
-student_list = []
-course_list = []
+def input_number_of_students():
+    return get_number('Enter number of students: ')
 
 
-def input_number_of_student():
-    global studentCount
-    studentCount = get_number('Enter number of students: ')
-
-
-def add_student():
-    for i in range(studentCount):
+def add_student(count):
+    for i in range(count):
         print(f'Enter information for student {i + 1}:')
         name = input('- Student name: ')
-        id = input('- Student ID: ')
+        sid = input('- Student ID: ')
         DoB = input('- Date of Birth: ')
 
-        student = (name, id, DoB)  # tuple
-
-        student_list.append(student)  # add tuple as an element to end of list
-        print('\n')
+        student = {
+            'Name': name,
+            'ID': sid,
+            'Date of birth': DoB
+        }
+        students.append(student)
+    return students
 
 
 def input_number_of_courses():
-    global courseCount
-    courseCount = get_number('Enter number of courses: ')
+    return get_number('Enter number of courses: ')
 
 
-def add_course():
-    for i in range(courseCount):
-        print(f'Enter information for course {i + 1}:')
-        id = input('- Course ID: ')
+def add_course(count):
+    for i in range(count):
+        print(f'Enter information for course {i+1}:')
+        cid = input('- Course ID: ')
         name = input('- Course name: ')
 
-        course = (id, name)
-
-        course_list.append(course)  # add tuple as an element to end of list
-        print('\n')
+        course = {
+            'ID': cid,
+            'Name': name
+        }
+        courses.append(course)
+    return courses
 
 
 def input_mark_for_course():
@@ -58,24 +59,31 @@ def input_mark_for_course():
     pass
 
 
-def list_course():
-    [print(record) for record in course_list]
-
-
 def list_student():
-    # for loop
-    # loop through list, print student names
-    [print(record) for record in student_list]
+    print("\nStudent list")
+    for student in students:
+        print(f"{student['Name']: <20} {student['ID']: <10} {student['Date of birth']}")
+
+
+def list_course():
+    print("\nCourse list")
+    for course in courses:
+        print(f"{course['ID']: <10} {course['Name']}")
 
 
 def show_mark():
     pass
 
 
-input_number_of_student()
-add_student()
+students = []
+courses = []
+
+# Enter number of students, info of students, list them
+studentCount = input_number_of_students()
+add_student(studentCount)
 list_student()
 
-input_number_of_courses()
-add_course()
+# Enter number of courses, info of courses, list them
+courseCount = input_number_of_courses()
+add_course(courseCount)
 list_course()
